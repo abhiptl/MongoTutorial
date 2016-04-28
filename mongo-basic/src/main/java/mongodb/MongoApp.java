aec9290d76d4abf1a00f16c05cd4cd9456ed33b0
abhiptl <abhishek.patel@sanelib.com>
2016-04-28 22:21:09 +0530

    Addind mongo crud.

--- mongo-basic/src/main/java/mongodb/MongoApp.java
+++ mongo-basic/src/main/java/mongodb/MongoApp.java
@@ -5,6 +5,6 @@ public class MongoApp
 {
     public static void main( String[] args )
     {
-        System.out.println("Welcome to MongoDB World !!" );
+        System.out.println("Welcome to MongoDB CRUD Module !!" );
     }
 }
