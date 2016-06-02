c16481ce77b8d7cbc866a3d2f086980eb1f61520
abhiptl <abhishek.patel@sanelib.com>
2016-06-02 21:07:18 +0530

    Weeke Challenge Problem2 solution.

---
+++ mongo-university/src/main/java/com/abhishek/mongodb/homework/week2/ChallengeProblem2.java
@@ -0,0 +1,31 @@
+package com.abhishek.mongodb.homework.week2;
+
+import com.abhishek.mongodb.constant.MongoUtil;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static java.util.Arrays.asList;
+
+/**
+ * Created by Abhishek on 6/2/2016.
+ */
+public class ChallengeProblem2 {
+
+  public static void main(String[] args) {
+	MongoClient client = new MongoClient();
+
+	MongoDatabase database = client.getDatabase("video");
+	final MongoCollection<Document> movieDetailsCollection = database.getCollection("movieDetails");
+
+	FindIterable<Document> documents = movieDetailsCollection.find(and(Filters.lt("imdb.votes", 10000) ,Filters.eq("tomato.consensus", null), Filters.in("year", asList(2010,2011,2012,2013,2014))));
+	MongoUtil.printFindIterableDocuments(documents);
+
+  }
+
+}
